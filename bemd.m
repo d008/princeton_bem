0afb202ac034a9196ae7da11bd251dc942988468
Mark Miller <millerma@princeton.edu>
2018-05-10 18:08:18 -0400

    Some changes
    Overhauled how runconditions were input and how data generated by a simulation was stored.

---
+++ bemd.m
@@ -0,0 +1,24 @@
+classdef bemd < pbem_cls
+    %Defines results of a pbem run and some basic operations
+    %   Detailed explanation goes here
+    
+    properties
+        Ct
+        Cp
+        ReD
+        Retip
+        fx
+        Power
+        torq
+        U
+        rho
+        mu
+        omega
+        speed
+    end
+    
+    methods
+    end
+    
+end
+
